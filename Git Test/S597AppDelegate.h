cf10a649be58d67400143405f2b5aa4b1b04c261
Chase <rchasebell@gmail.com>
2014-07-30 19:27:52 -0700

    first commit

---
+++ Git Test/S597AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  S597AppDelegate.h
+//  Git Test
+//
+//  Created by Chase Bell on 7/30/14.
+//  Copyright (c) 2014 597 Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface S597AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/S597ViewController.h
@@ -0,0 +1,13 @@
+//
+//  S597ViewController.h
+//  Git Test
+//
+//  Created by Chase Bell on 7/30/14.
+//  Copyright (c) 2014 597 Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface S597ViewController : UIViewController
+
+@end
