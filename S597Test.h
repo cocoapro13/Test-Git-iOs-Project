80d1b9ed08c5c3298c2f27e5634b9477e51b08e1
Chase <rchasebell@gmail.com>
2014-07-30 19:33:57 -0700

    added Test Class

---
+++ S597Test.h
@@ -0,0 +1,13 @@
+//
+//  S597Test.h
+//  Git Test
+//
+//  Created by Chase Bell on 7/30/14.
+//  Copyright (c) 2014 597 Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface S597Test : NSObject
+
+@end
---
+++ S597Test.m
@@ -0,0 +1,13 @@
+//
+//  S597Test.m
+//  Git Test
+//
+//  Created by Chase Bell on 7/30/14.
+//  Copyright (c) 2014 597 Studios. All rights reserved.
+//
+
+#import "S597Test.h"
+
+@implementation S597Test
+
+@end
